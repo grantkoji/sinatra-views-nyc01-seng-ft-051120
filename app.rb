f35f4dc090c8dc7d7131569037bb1fab694ce97d
grantkoji <grantkoji@gmail.com>
2020-05-31 13:44:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	#update our controller to render the index.erb
+	#file at the "/" route. 
+	#tells Sinatra to render a file called index.erb inside
+	#of a directory called views
 	get '/' do
-		
+		erb :index	
+	end
+
+	get '/info' do
+		erb :info
 	end
 end
